a57575fe8ef13867be547654a8ba67264122664f
Feisal Lam-Lion <feisal@lamlion.com>
2017-03-31 00:12:13 +0200

    update

---
+++ rds_vars.tf
@@ -0,0 +1,42 @@
+variable "identifier" {
+  default     = "odoo-rds"
+  description = "Identifier for your DB"
+}
+
+variable "storage" {
+  default     = "10"
+  description = "Storage size in GB"
+}
+
+variable "engine" {
+  default     = "postgres"
+  description = "Engine type, example values mysql, postgres"
+}
+
+variable "engine_version" {
+  description = "Engine version"
+
+  default = {
+    mysql    = "5.6.22"
+    postgres = "9.6.2"
+  }
+}
+
+variable "instance_class" {
+  default     = "db.t2.micro"
+  description = "Instance class"
+}
+
+variable "db_name" {
+  default     = "mydb"
+  description = "mydb name"
+}
+
+variable "username" {
+  default     = "dbadmin"
+  description = "dbadmin username"
+}
+
+variable "password" {
+  description = "password, provide through your ENV variables"
+}
